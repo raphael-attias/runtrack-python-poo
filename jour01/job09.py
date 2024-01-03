b25c39836385d6f0e17b34b435e0282cd38e556f
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2024-01-03 08:22:01 +0100

    .

--- jour01/job09.py
+++ jour01/job09.py
@@ -14,12 +14,12 @@ class Produit:
     def calculerPrixTTC(self):
         return self.prixHT * (1 + self.TVA)
 
-    def ObtenirInformations(self):
-        informations = f"Nom du produit : {self.nom}\n"
-        informations += f"Prix HT : {self.prixHT} €\n"
-        informations += f"TVA : {self.TVA * 100}%\n"
-        informations += f"Prix TTC : {self.calculerPrixTTC()} €"
-        return informations
+    def infos_generale(self):
+        infos = f"Nom du produit : {self.nom}\n"
+        infos += f"Prix HT : {self.prixHT} €\n"
+        infos += f"TVA : {self.TVA * 100}%\n"
+        infos += f"Prix TTC : {self.calculerPrixTTC()} €"
+        return infos
 
     def modifierNom(self, nouveau_nom):
         self.nom = nouveau_nom
@@ -39,12 +39,12 @@ class Produit:
 produit1 = Produit("Produit_X", 10, 20)
 produit2 = Produit("Produit_Y", 20, 15)
 
-informations_produit1 = produit1.ObtenirInformations()
-informations_produit2 = produit2.ObtenirInformations()
+infos_produit1 = produit1.ObtenirInformations()
+infos_produit2 = produit2.ObtenirInformations()
 
 print("Informations initiales des produits :")
-print(informations_produit1)
-print(informations_produit2)
+print(infos_produit1)
+print(infos_produit2)
 
 produit1.modifierNom("Nouveau Produit_X")
 produit1.modifierPrix(15)
@@ -53,9 +53,9 @@ produit2.modifierNom("Nouveau Produit_Y")
 produit2.modifierPrix(25)
 
 # Obtention prix après TVA
-informations_modifiees_produit1 = produit1.ObtenirInformations()
-informations_modifiees_produit2 = produit2.ObtenirInformations()
+modif_produit1 = produit1.ObtenirInformations()
+modif_produit2 = produit2.ObtenirInformations()
 
 print("Informations des produits après TVA :")
-print(informations_modifiees_produit1)
-print(informations_modifiees_produit2)
+print(modif_produit1)
+print(modif_produit2)
