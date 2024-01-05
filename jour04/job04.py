50c5af4d1ee99ecdfc53e79629c71a05dd12c7ee
raphael.attias <raphael.attias@laplateforme.io>
2024-01-05 11:14:04 +0100

    +j04

---
+++ jour04/job04.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Made by: Raphael
+Email: raphael.attias@laplateforme.io
+"""
+
+class Forme:
+    def aire(self):
+        return 0
+
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def aire(self):
+        return self.largeur * self.hauteur
+        
+rectangle1 = Rectangle(largeur=5, hauteur=3)
+
+resultat_aire = rectangle1.aire()
+
+print("L'aire du rectangle est:", resultat_aire)
